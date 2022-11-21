c34c3464bd8b851499a0601aa696d2b47db37e65
faisal khan <64373164+devfaisalkhan@users.noreply.github.com>
2022-11-21 18:38:44 +0500

    Initial Commit

---
+++ Donation Ease/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Donation Ease
+//
+//  Created by Faisal Khan on 21/11/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
